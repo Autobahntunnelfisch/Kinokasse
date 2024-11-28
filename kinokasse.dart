333001e57400aba403e2d438e7fe449beeec2aa8
Autobahntunnelfisch <cankinik@web.de>
2024-11-28 15:44:23 +0100

    add comments

--- kinokasse.dart
+++ kinokasse.dart
@@ -4,12 +4,12 @@ int platzNummer = 50000;
 int plaetzeProReihe = 12;
 
 
-int reihe = (platzNummer - 1) ~/ plaetzeProReihe +1;
-int platz = (platzNummer - 1) % plaetzeProReihe +1;
+int reihe = (platzNummer - 1) ~/ plaetzeProReihe +1;    //Ergebnis + 1 ergibt die Reihe
+int platz = (platzNummer - 1) % plaetzeProReihe +1;     //Rest + 1 ergibt Sitzplatz
 
 int preis;
 
-if (platz % 2 == 0) {
+if (platz % 2 == 0) {    //legt Fest ob ungerade oder gerade
   preis = 15;
 } else {
   preis = 12;
